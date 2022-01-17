e4fd34e5e75c22f953399ac4412934a48100c7bc
Dylan Miska <dmiska@bushelpowered.com>
2022-01-16 21:17:31 -0600

    minor response adjustments to conform to app frontend.

--- Robo-Pantry-API/src/main/kotlin/com/dylanmiska/RoboPantryAPI/adapter/web/contract/response/product/Maps.kt
+++ Robo-Pantry-API/src/main/kotlin/com/dylanmiska/RoboPantryAPI/adapter/web/contract/response/product/Maps.kt
@@ -7,6 +7,7 @@ import com.dylanmiska.RoboPantryAPI.core.domain.model.ProductVariant
 fun Product.toResponse(): ProductResponse = ProductResponse(
     id = id!!,
     name = name,
+    category = category,
     unitsOnHand = unitsOnHand!!,
     unitOfMeasure = unitOfMeasure,
     productVariants = productVariants.map(ProductVariant::toResponse)
@@ -15,6 +16,7 @@ fun Product.toResponse(): ProductResponse = ProductResponse(
 fun Product.toListResponse(): ProductListResponse = ProductListResponse(
     id = id!!,
     name = name,
+    category = category,
     unitsOnHand = unitsOnHand!!,
     unitOfMeasure = unitOfMeasure
 )
--- Robo-Pantry-API/src/main/kotlin/com/dylanmiska/RoboPantryAPI/common/enums/ProductCategory.kt
+++ Robo-Pantry-API/src/main/kotlin/com/dylanmiska/RoboPantryAPI/common/enums/ProductCategory.kt
@@ -1,16 +1,30 @@
 package com.dylanmiska.RoboPantryAPI.common.enums
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 enum class ProductCategory {
+    @JsonProperty("beverage")
     BEVERAGE,
+    @JsonProperty("bakery")
     BAKERY,
+    @JsonProperty("canned_good")
     CANNED_GOOD,
+    @JsonProperty("dairy")
     DAIRY,
+    @JsonProperty("dry_good")
     DRY_GOOD,
+    @JsonProperty("frozen")
     FROZEN,
+    @JsonProperty("meat")
     MEAT,
+    @JsonProperty("produce")
     PRODUCE,
+    @JsonProperty("cleaner")
     CLEANER,
+    @JsonProperty("paper_good")
     PAPER_GOOD,
+    @JsonProperty("personal_care")
     PERSONAL_CARE,
+    @JsonProperty("other")
     OTHER
 }
